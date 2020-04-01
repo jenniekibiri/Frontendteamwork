05aff57a84690a9fc50a909813f22e146734c979
jenniekibiri <josphatje@gmail.com>
2020-04-01 16:35:08 +0300

    initial commit

--- index.js
+++ index.js
@@ -1,56 +1,122 @@
 const express = require('express');
+const ejs = require('ejs');
+const mysql = require('mysql');
 const path = require('path');
 const router = express.Router();
 const bodyparser = require('body-parser');
-const { Client } = require('pg');
-const connectionString = 'postgres://postgres:5463@localhost:5432/teamwork';
+const {Client} = require('pg');
+
+const env = process.env.DATABASE_URL
+const connectionString = ' env || postgresql://postgres:5463jeny@localhost:5432/teamwork';
 const client = new Client({
     connectionString: connectionString
 });
 client.connect();
-
-
 const app = express();
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({
+    extended: false
+}));
 app.use(express.static('public'))
 
-
+app.set('view engine', 'ejs');
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname + '/public/login.html'));
-});
-app.get('/feeds', (req, res) => {
-    res.sendFile(path.join(__dirname + '/public/feeds.html'));
-});
-app.get('/signup', (req, res) => {
-    res.sendFile(path.join(__dirname + '/public/signup.html'));
-});
-app.get('/drafts', (req, res) => {
-    res.sendFile(path.join(__dirname + '/public/drafts.html'));
-});
-app.get('/settings', (req, res) => {
-    res.sendFile(path.join(__dirname + '/public/settings.html'));
-});
-
-app.get('/signup', (req, res) => {
-    res.sendFile(path.join(__dirname + '/public/signup.html'));
+    res.render('login')
 });
 
 app.get('/feeds', (req, res) => {
-    res.sendFile(path.join(__dirname + '/public/myarticles.html'))
+
+    let sql = 'select * from articles inner join account on articles.author_id=account.user_id ORDER BY created_on';
+    client.query(sql, (err, result) => {
+
+
+        if (err) throw err;
+        res.render('feeds', {
+            title: 'how to deal with programming burnouts',
+            articles: result.rows
+
+
+        })
+    });
+   
+
+});
+
+
+
+app.get('/signup', (req, res) => {
+    res.render('signup')
+});
+
+
+
+
+
+app.get('/draft', (req, res) => {
+    res.render('draft')
+});
+
+
+app.get('/comments', (req, res) => {
+   
+
+    client.query(`insert into account () values('${username}','${email}','${jobrole}','${password}','${department}')`, (err, result) => {
+        if (err) {
+
+            console.log(err);
+
+        } else {
+
+            res.redirect('/feeds')
+        }
+    })
+});
+
+
+app.post('/articles', (req, res) => {
+    var userid = req.body.userid;
+    var title = req.body.title;
+    var category = req.body.category;
+    var article = req.body.article;
+    var dateCreated = new Date();
+    var dd = dateCreated.getDate()
+    var mm = dateCreated.getMonth() + 1
+
+    var yyyy = dateCreated.getFullYear()
+    if (dd < 10) {
+        dd = '0' + dd;
+    }
+    if (mm < 10) {
+        mm = '0' + mm;
+    }
+    dateCreated = yyyy + mm + dd
+  
+    client.query(`insert into articles (author_id,article_title,category,article,created_on) values('${userid}','${title}','${category}','${article}','${dateCreated}')`, (err, result) => {
+        if (err) {
+
+            console.log(err);
+
+        } else {
+
+            res.redirect('/feeds')
+        }
+    })
+
 })
 
 app.post('/login', (req, res) => {
     var password = req.body.password;
     var username = req.body.username;
-        if (username && password) {
-        client.query(`select * from account where username = '${req.body.username}' AND  password = '${req.body.password}'`, (err, result) => {
+    var email = req.body.username;
+
+
+    if (username && password) {
+        client.query(`select * from account where username = '${req.body.username}' OR email ='${req.body.username}' AND  password = '${req.body.password}'`, (err, result) => {
             const rows = result.rows;
-            console.log(result.rows)
-            console.log(typeof result);
-            console.log(rows.length)
+
             if (rows.length > 0) {
                 res.redirect('/feeds')
+
             } else {
                 res.send('invalid username and password');
             }
@@ -59,6 +125,7 @@ app.post('/login', (req, res) => {
     } else {
         res.send('enter credentials')
     }
+
 
 })
 
@@ -75,12 +142,14 @@ app.post('/signup', (req, res) => {
 
             console.log(err);
 
-        }
-        else {
-           
+        } else {
+
             res.redirect('/')
         }
     })
 });
+
+
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`server is running on port ${PORT}`))
---
+++ views/crap.js
@@ -0,0 +1,5 @@
+<%for(i=0; i<articles.length; i++){%>
+    <h3><%= articles[i].article_title%></h3>   
+    <p><%= articles[i].article%></p>  
+  
+  
